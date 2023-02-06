41856eada9d31dd1bea5797c5123210b8c13cacc
myk6 <myk6@fresnou.org>
2023-02-06 13:43:02 -0800

    initial commit

---
+++ DataTypes.js
@@ -0,0 +1,44 @@
+console.log(7);
+
+//this is a comment
+
+//numbers
+//integers
+console.log(-3);
+console.log(0);
+console.log(7);
+
+//floating point numbers(floats)
+console.log(3.14);
+console.log(-0.123456);
+//strings
+//characters, letters, symbols, spaces
+console.log("Hello World");
+console.log('!@#$ is also a string');
+console.log(`backticks by 1`);
+
+//arithmetic operators (math)
+console.log(14+2);111``
+console.log(14-2);
+console.log(14*2); //shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is called MOD
+                   //it means remainder after dividing
+
+//concatenation means to combine strings
+console.log("my name"+"is patrick.");
+console.log('My'+' '+'age'+' '+'is'+14+'.');
+console.log("Your name is\n16.");
+
+//Template literal - plug in some calculated value
+//Use backticks `` by the 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10} `);
+
+console.log(`Your age is ${14+3}`);
+console.log(`15 divided by 10 = ${15/10} `);
+console.log(`15 divided by 10 = ${15/10} `);
+console.log(`15 divided by 10 = ${15/10} `);
+
+
+console.log(17+2);
+console.log(
